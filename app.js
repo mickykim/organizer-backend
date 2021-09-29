41d7740f8423d216b32a873a4370087c5a406efc
Micky Kim <miqueaskimhan@gmail.com>
2021-09-29 18:07:50 -0500

    Updated CORS Policy

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // Parse incoming requests with JSON payloads
 // Enable CORS
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
